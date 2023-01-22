aa79a6f6e1ad7a651ae3aa8ca39d43fe1a02aa28
Facundo David Gonzalez <facundo.gonzalez@devactory.com>
2023-01-22 12:19:12 -0300

    add cors and fix dependencies

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { connectDb } from "./src/db";
 import { config } from "./src/config";
 import todoRouter from "./src/routes/todo";
@@ -8,13 +9,9 @@ const app = express();
 
 void connectDb();
 
-// app.use(jwtCheck);
-
+app.use(cors());
+app.use(jwtCheck);
 app.use("/api/todos", todoRouter);
-
-app.get("/test", jwtCheck, (_req, res) => {
-  res.send("test");
-});
 
 app.listen(config.PORT, () => {
   console.info("server started on port 3000");
